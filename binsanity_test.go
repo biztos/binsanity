856a5ab2188a5b65d0e3438d5751a1bcbe7632f6
Kevin Frost <biztos@mac.com>
2017-02-14 01:19:03 +0100

    Working… ish.

--- binsanity_test.go
+++ binsanity_test.go
@@ -5,9 +5,12 @@
 package binsanity_test
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/biztos/binsanity"
@@ -22,13 +25,70 @@ func Test_Process_Basic(t *testing.T) {
 
 	pkg := "bstest"
 	src := filepath.Join("test", "files")
-	destfile := filepath.Join(dir, "bstest")
+	destfile := filepath.Join(dir, "bstest.go")
 
 	// We don't actually know our import path so we might as well guess.
 	// (No idea how this will play with CI, but Travis will let us know.)
 	err = binsanity.Process(src, pkg, "", destfile)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal(err.Error())
 	}
+
+	pinfo, err := os.Stat(destfile)
+	if err != nil {
+		t.Fatalf("Package file stat error for %v", err)
+	}
+	if pinfo.IsDir() {
+		t.Fatalf("Package file is a dir... WTF?")
+	}
+	tinfo, err := os.Stat(filepath.Join(dir, "bstest_test.go"))
+	if err != nil {
+		t.Fatalf("Package file stat error for %v", err)
+	}
+	if tinfo.IsDir() {
+		t.Fatalf("Test file is a dir... WTF?")
+	}
+
+	// Now the really crazy thing is to go in and run the tests.
+	if err := RunTestsInDir(t, dir); err != nil {
+		t.Fatalf("Bad test results: %v", err)
+	}
+
+}
+
+// Run tests with coverage.  Anything other than 100% is an error.
+func RunTestsInDir(t *testing.T, dir string) error {
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic("failed to Getwd: " + err.Error())
+	}
+	if err := os.Chdir(dir); err != nil {
+		panic("failed to Chdir: " + err.Error())
+	}
+	defer os.Chdir(cwd)
+
+	cmd := "go"
+	args := []string{"test", "--cover"}
+	output, err := exec.Command(cmd, args...).Output()
+	if err != nil {
+		return err
+	}
+
+	// We expect something like this:
+	// PASS
+	// coverage: 100% of statements
+	// ok      github.com/biztos/binsanity    0.010s
+	//
+	// However we probably don't want to assume too much about newlines. Hack:
+	f := strings.Fields(string(output))
+	if len(f) < 3 || f[0] != "PASS" {
+		return fmt.Errorf("Tests did not PASS: %s", string(output))
+	}
+	if f[1] != "coverage:" || f[2] != "100.0%" {
+		return fmt.Errorf("Coverage seems low: %s", string(output))
+	}
+
+	return nil
 
 }
